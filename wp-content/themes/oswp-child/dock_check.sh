98572d9b85e2dd7ea3e7fa79f16524c799cfefbd
omegasemantic <siom.kearns@posteo.net>
2026-08-04 12:49:57 +1200

    vps ready

--- wp-content/themes/oswp-child/dock_check.sh
+++
@@ -1,1 +0,0 @@
-docker exec -i oswp-wordpress-1 php -l /var/www/html/wp-content/themes/oswp-child/page-events-schema.php
--- wp-content/themes/oswp-child/template-parts/wrap-open.php
+++
@@ -1,2 +0,0 @@
-<main class="wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained" style="margin-top:var(--wp--preset--spacing--60)">
-  <div class="wp-block-group alignfull has-global-padding is-layout-constrained wp-block-group-is-layout-constrained" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">ihello</div>
